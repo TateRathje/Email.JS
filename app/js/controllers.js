85244a3a243cfb8ef214bd7e3856c0d449260088
Tate Rathje <taterathje@gmail.com>
2014-07-20 05:37:03 -0700

    Continued building out index.html and controllers.js, added tabs for inbox and sent, added compose modal.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,13 @@ angular.module('myApp.controllers', [])
 
 	.controller('EmailCtrl', function($scope) {
 		$scope.isPopupVisible = false;
+		$scope.isComposePopupVisible = false;
+		$scope.composeEmail = {};
+		$scope.activeTab = "inbox";
+		$scope.sentEmails = [];
 
+		// Email Popup Controlles
+		
 		$scope.showPopup = function (email) {
 			$scope.isPopupVisible = true;
 			$scope.selectedEmail = email;
@@ -10,6 +16,22 @@ angular.module('myApp.controllers', [])
 
 		$scope.closePopup = function () {
 			$scope.isPopupVisible = false;
+		};
+
+		// Compose Popup Controlles
+
+		$scope.sendEmail = function() {
+			$scope.isComposePopupVisible = false;
+			$scope.sentEmails.push($scope.composeEmail);
+		};
+
+		$scope.showComposePopup = function () {
+			$scope.composeEmail = {};
+			$scope.isComposePopupVisible = true;
+		};
+
+		$scope.closeComposePopup = function () {
+			$scope.isComposePopupVisible = false;
 		};
 
 		$scope.emails = [
