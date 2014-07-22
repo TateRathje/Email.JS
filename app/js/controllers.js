26d3a5855ba294510c7e588ca529ee68341ffb44
Tate Rathje <taterathje@gmail.com>
2014-07-22 01:28:05 -0700

    Finished email.js app, fixed sent tab, added reply and forward buttons with functionality on all sent emails.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,7 +22,9 @@ angular.module('myApp.controllers', [])
 
 		$scope.sendEmail = function() {
 			$scope.isComposePopupVisible = false;
-			$scope.sentEmails.push($scope.composeEmail);
+			$scope.sentEmails.splice(0,0,$scope.composeEmail);
+			$scope.composeEmail.date = new Date();
+			$scope.composeEmail.from = "Me";
 		};
 
 		$scope.showComposePopup = function () {
@@ -34,20 +36,63 @@ angular.module('myApp.controllers', [])
 			$scope.isComposePopupVisible = false;
 		};
 
+		// Forward Email
+
+		$scope.forward = function() {
+			$scope.isPopupVisible = false;
+			$scope.composeEmail = {};
+			angular.copy($scope.selectedEmail, $scope.composeEmail);
+			$scope.composeEmail.body =
+				"\n----------------------\n"
+				+ "from: " + $scope.composeEmail.from + "\n"
+				+ "sent: " + $scope.composeEmail.date + "\n"
+				+ "to: "   +                            "\n"
+				+ "subject: " + $scope.composeEmail.subject + "\n"
+				+ $scope.composeEmail.body;
+		  $scope.composeEmail.subject = "FW: " + $scope.composeEmail.subject;
+		  $scope.composeEmail.from = "me";
+		  $scope.isComposePopupVisible = true;
+		};
+
+		// Reply Email
+
+		$scope.reply = function() {
+			$scope.isPopupVisible = false;
+			$scope.composeEmail = {};
+			angular.copy($scope.selectedEmail, $scope.composeEmail);
+			$scope.composeEmail.body =
+				"\n----------------------\n"
+				+ "from: " + $scope.composeEmail.from + "\n"
+				+ "sent: " + $scope.composeEmail.date + "\n"
+				+ "to: " + $scope.composeEmail.to + "\n"
+				+ "subject: " + $scope.composeEmail.subject + "\n"
+				+ $scope.composeEmail.body;
+		  $scope.composeEmail.subject = "RE: " + $scope.composeEmail.subject;
+		  $scope.composeEmail.to = $scope.composeEmail.from;
+		  $scope.composeEmail.from = "me";
+		  $scope.isComposePopupVisible = true;
+		};
+
+		// Static Emails
+
+
 		$scope.emails = [
 					{ 
+						to: 'me',
 						from: 'John', 
 						subject: 'I love angular', 
 						date: 'Jan 1', 
 						body: 'hello world'
 					},
 					{ 
+						to: 'me',
 						from: 'Jack', 
 						subject: 'Angular and I are just friends', 
 						date: 'Fed 15',
 						body: 'just kidding' 
 					},
 					{ 
+						to: 'me',
 						from: 'Ember', 
 						subject: 'I hate you Angular', 
 						date: 'Dec 8',
